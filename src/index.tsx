5cd6934253cc40772513b6948e99fde9b5c71cc2
daniel-motta <daniel.motta@compasso.com.br>
2022-02-07 14:58:01 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+  <SafeAreaView>
+    <Text>Hello World!</Text>
+  </SafeAreaView>
+);
+}
+
+export default App;
