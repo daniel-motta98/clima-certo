ad2aa01d85830adcf0c9183e0c1c1a4adce75099
daniel-motta <daniel.motta@compasso.com.br>
2022-02-11 00:04:51 -0300

    last commit

--- src/screens/Cities/index.tsx
+++ src/screens/Cities/index.tsx
@@ -1,79 +1,31 @@
-import React, { useState } from 'react';
-import { Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
-import Input from '../../components/Input';
+import { useNavigation } from '@react-navigation/native';
 
 import themes from '../../global/themes';
 
-import CardForecast from '../../components/CardForecast';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+import api from '../../services/api';
 
 import * as  S from './styles';
-import { useNavigation } from '@react-navigation/native';
+import { KeyboardAvoidingView } from 'react-native';
 
 const Cities: React.FC = () => {
 
   const { navigate } = useNavigation();
-  const [searchCity, setSearchCity] = useState('');
-  const [backgroundUrl, setBackgroundUrl] = useState('https://p0.piqsels.com/preview/622/143/689/4k-wallpaper-clouds-cloudy-dark.jpg')
-  const [city, setCity] = useState([
-    {
-      id: 1,
-      name: 'Alto Alegre',
-      temperature: 18,
-      subtitle: 'Brasil',
-      typeRain: 'Chuva Leve',
-      min: 18,
-      max: 30,
-    },
-    {
-      id: 2,
-      name: 'Porto Alegre',
-      temperature: 18,
-      subtitle: 'Brasil',
-      typeRain: 'Chuva Forte',
-      min: 4,
-      max: 12,
-    },
-    {
-      id: 3,
-      name: 'Orlando',
-      temperature: 18,
-      subtitle: 'US',
-      typeRain: 'Tempestade',
-      min: 14,
-      max: 26,
-    },
-    {
-      id: 4,
-      name: 'Toronto',
-      temperature: 18,
-      subtitle: 'Canadá',
-      typeRain: 'Chuva Leve',
-      min: -2,
-      max: 5,
-    },
-    {
-      id: 5,
-      name: 'Páris',
-      temperature: 18,
-      subtitle: 'França',
-      typeRain: 'Alerta de Granizo',
-      min: 17,
-      max: 29,
-    },
-    {
-      id: 6,
-      name: 'Blumenau',
-      temperature: 18,
-      subtitle: 'Brasil',
-      typeRain: 'Chuva Forte',
-      min: 14,
-      max: 26,
-    },
-  ]);
+  const [backgroundUrl, setBackgroundUrl] = useState('https://p0.piqsels.com/preview/622/143/689/4k-wallpaper-clouds-cloudy-dark.jpg');
+  const [name, setName] = useState('');
+  const [currentTemperature, setCurrentTemperature] = useState(0);
+  const [country, setCountry] = useState('');
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(0);
+  const [favorite, setFavorite] = useState(false);
+  const [nameSearch, setNameSearch] = useState('');
 
   const handleAddCity = (id: any) => {
-    Alert.alert('oi', 'oi');
+    setFavorite(!favorite)
   }
 
   const handleToGoInformation = () => {
@@ -81,47 +33,117 @@ const Cities: React.FC = () => {
     navigate('InformationWeather');
   }
 
-  const searchCities = city.filter((item: any) => {
-    return item.name.indexOf(searchCity) >= 0;
-  });
-
-  const renderItem = ({ item }: any) => {
-    return (
-      <CardForecast
-        labelTitle={item.name}
-        currentTemperature={item.temperature}
-        labelSubtitle={item.subtitle}
-        typeRain={item.typeRain}
-        iconLikeName={'hearto'}
-        iconLikeColor={themes.colors.danger200}
-        iconLikeSize={32}
-        minTemparature={item.min}
-        maxTemperature={item.max}
-        labelButtonBlue={'PRÓXIMAS PREVISÕES'}
-        callBackButtonBlue={handleToGoInformation}
-      />
-    );
+  const getNameCitySearch = (data: any) => {
+    setNameSearch(data.description);
   }
 
-  return (
-    <S.Container>
-      <S.ImageCustom source={{ uri: backgroundUrl }} />
-      <S.BoxHeader>
-        <Input placeholder='Pesquise pelo nome da cidade' value={searchCity} onChangeText={text => setSearchCity(text)} nameIcon={'search'} colorIcon={themes.colors.constrast1000} sizeIcon={32} />
-      </S.BoxHeader>
-      {searchCities.length === 0
-        ?
-        <S.BoxLabelNoCities>
-          <S.LabelBold>Ops... A cidade que você digitou não existe. 😕</S.LabelBold>
-        </S.BoxLabelNoCities>
-        :
-        <S.FlatListCustom
-          data={searchCities}
-          keyExtractor={(item: any) => item.id}
-          renderItem={renderItem}
-        />
+
+  useEffect(() => {
+    const getCity = async () => {
+      try {
+        if (nameSearch) {
+          const response = await api.get(`/?q=${nameSearch}&appid=a788f547d04e1ae0fac1ecd5419ae2c1&units=metric`);
+          const { data } = response;
+          setName(data.name);
+          setCurrentTemperature(data.main.temp);
+          setCountry(data.sys.country);
+          setMin(data.main.temp_min);
+          setMax(data.main.temp_max);
+        }
+      } catch (err) {
+        console.log('Deu erro', err);
       }
-    </S.Container>
+    }
+    getCity();
+  }, [nameSearch]);
+
+  return (
+    <>
+      <S.Container>
+        <S.ImageCustom source={{ uri: backgroundUrl }} />
+        <GooglePlacesAutocomplete
+          placeholder='Producrar por cidade'
+          onPress={(data) => getNameCitySearch(data)}
+          query={{
+            key: 'AIzaSyARGgDWIbFmrvoKbhzgZ7VE8Azj_hzrpco',
+            language: 'en',
+          }}
+          styles={{
+            textInput: {
+              margin: 20,
+              borderRadius: 10,
+              justifyContent: 'center',
+              zIndex: 999,
+            }
+          }}
+        />
+        {!nameSearch && (
+          <S.BoxCities style={{ display: 'none' }}>
+            <S.BoxLabelNameCityAndForecast>
+              <S.LabelNameCity>{name}</S.LabelNameCity>
+              <S.CurrentTemperature>{currentTemperature.toFixed(0)} ºC</S.CurrentTemperature>
+            </S.BoxLabelNameCityAndForecast>
+            <S.LabelSubtitle>{country}</S.LabelSubtitle>
+            <S.BoxLabelTypeRainAndButtonAdd>
+              <S.LabelMinAndMaxForecast>min: {min.toFixed(0)}º - max: {max.toFixed(0)}º</S.LabelMinAndMaxForecast>
+              <S.CustomButtonAddCity onPress={handleAddCity}>
+                <S.BoxIconFavorite>
+                  <Icon name={'hearto'} color={themes.colors.danger200} size={32} />
+                </S.BoxIconFavorite>
+              </S.CustomButtonAddCity>
+            </S.BoxLabelTypeRainAndButtonAdd>
+            <S.BoxNextForecast>
+              <S.LabelNextForecast onPress={handleToGoInformation}>PRÓXIMAS PREVISÕES</S.LabelNextForecast>
+            </S.BoxNextForecast>
+          </S.BoxCities>
+        )}
+        {nameSearch !== '' && (
+          <>
+            {favorite
+              ?
+              <S.BoxCitiesFavorite style={{ display: 'flex' }}>
+                <S.BoxLabelNameCityAndForecast>
+                  <S.LabelNameCity>{name}</S.LabelNameCity>
+                  <S.CurrentTemperature>{currentTemperature.toFixed(0)} ºC</S.CurrentTemperature>
+                </S.BoxLabelNameCityAndForecast>
+                <S.LabelSubtitle>{country}</S.LabelSubtitle>
+                <S.BoxLabelTypeRainAndButtonAdd>
+                  <S.LabelMinAndMaxForecast>min: {min.toFixed(0)}º - max: {max.toFixed(0)}º</S.LabelMinAndMaxForecast>
+                  <S.CustomButtonAddCity onPress={handleAddCity}>
+                    <S.BoxIconFavorite>
+                      <Icon name={'heart'} color={themes.colors.danger200} size={32} />
+                    </S.BoxIconFavorite>
+                  </S.CustomButtonAddCity>
+                </S.BoxLabelTypeRainAndButtonAdd>
+                <S.BoxNextForecastFavorite>
+                  <S.LabelNextForecast onPress={handleToGoInformation}>PRÓXIMAS PREVISÕES</S.LabelNextForecast>
+                  <S.LabelFavorite>Favorito!</S.LabelFavorite>
+                </S.BoxNextForecastFavorite>
+              </S.BoxCitiesFavorite>
+              :
+              <S.BoxCities style={{ display: 'flex' }}>
+                <S.BoxLabelNameCityAndForecast>
+                  <S.LabelNameCity>{name}</S.LabelNameCity>
+                  <S.CurrentTemperature>{currentTemperature.toFixed(0)} ºC</S.CurrentTemperature>
+                </S.BoxLabelNameCityAndForecast>
+                <S.LabelSubtitle>{country}</S.LabelSubtitle>
+                <S.BoxLabelTypeRainAndButtonAdd>
+                  <S.LabelMinAndMaxForecast>min: {min.toFixed(0)}º - max: {max.toFixed(0)}º</S.LabelMinAndMaxForecast>
+                  <S.CustomButtonAddCity onPress={handleAddCity}>
+                    <S.BoxIconFavorite>
+                      <Icon name={'hearto'} color={themes.colors.danger200} size={32} />
+                    </S.BoxIconFavorite>
+                  </S.CustomButtonAddCity>
+                </S.BoxLabelTypeRainAndButtonAdd>
+                <S.BoxNextForecastFavorite>
+                  <S.LabelNextForecast onPress={handleToGoInformation}>PRÓXIMAS PREVISÕES</S.LabelNextForecast>
+                </S.BoxNextForecastFavorite>
+              </S.BoxCities>
+            }
+          </>
+        )}
+      </S.Container>
+    </>
   );
 }
 
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.openweathermap.org/data/2.5/',
+  baseURL: 'https://api.openweathermap.org/data/2.5/weather',
   headers: {
     'App-Token': 'url',
-    Referer: 'https://api.openweathermap.org/data/2.5/',
+    Referer: 'https://api.openweathermap.org/data/2.5/weather',
   },
 });
 
